5283653b12bf0083ea8a74c7ec8664cc099dd7c6
ryanjon2040 <mindfreak2040@gmail.com>
2020-07-26 18:45:19 +0530

    Fix unable to start without discord.

--- Source/DiscordUE4/Private/DiscordObject.cpp
+++ Source/DiscordUE4/Private/DiscordObject.cpp
@@ -53,68 +53,86 @@ const FString UDiscordObject::GetDiscordResultString(EDiscordReturnResult InDisc
 void UDiscordObject::SetState(FString InNewState)
 {
 	activity.SetState(TCHAR_TO_UTF8(*InNewState));
-	core->ActivityManager().UpdateActivity(activity, [](discord::Result result)
+	if (core)
 	{
-		uint8 ResultByte = (uint8)result;
-		DiscordObjectInstance->OnStateSet.Broadcast(static_cast<EDiscordReturnResult>(ResultByte));
-		LogDisplay(FString::Printf(TEXT("State Set Result: %s"), *GetDiscordResultString(static_cast<EDiscordReturnResult>(ResultByte))));
-	});
+		core->ActivityManager().UpdateActivity(activity, [](discord::Result result)
+		{
+			uint8 ResultByte = (uint8)result;
+			DiscordObjectInstance->OnStateSet.Broadcast(static_cast<EDiscordReturnResult>(ResultByte));
+			LogDisplay(FString::Printf(TEXT("State Set Result: %s"), *GetDiscordResultString(static_cast<EDiscordReturnResult>(ResultByte))));
+		});
+	}
 }
 
 void UDiscordObject::SetDetails(FString InNewDetails)
 {
 	activity.SetDetails(TCHAR_TO_UTF8(*InNewDetails));
-	core->ActivityManager().UpdateActivity(activity, [](discord::Result result)
+	if (core)
 	{
-		uint8 ResultByte = (uint8)result;
-		DiscordObjectInstance->OnDetailsSet.Broadcast(static_cast<EDiscordReturnResult>(ResultByte));
-		LogDisplay(FString::Printf(TEXT("Details Set Result: %s"), *GetDiscordResultString(static_cast<EDiscordReturnResult>(ResultByte))));
-	});
+		core->ActivityManager().UpdateActivity(activity, [](discord::Result result)
+		{
+			uint8 ResultByte = (uint8)result;
+			DiscordObjectInstance->OnDetailsSet.Broadcast(static_cast<EDiscordReturnResult>(ResultByte));
+			LogDisplay(FString::Printf(TEXT("Details Set Result: %s"), *GetDiscordResultString(static_cast<EDiscordReturnResult>(ResultByte))));
+		});
+	}
 }
 
 void UDiscordObject::SetLargeImage(const FString InKeyName)
 {
 	activity.GetAssets().SetLargeImage(TCHAR_TO_UTF8(*InKeyName));
-	core->ActivityManager().UpdateActivity(activity, [](discord::Result result)	
+	if (core)
 	{
-		uint8 ResultByte = (uint8)result;
-		DiscordObjectInstance->OnLargeImageSet.Broadcast(static_cast<EDiscordReturnResult>(ResultByte));
-		LogDisplay(FString::Printf(TEXT("Large Image Set Result: %s"), *GetDiscordResultString(static_cast<EDiscordReturnResult>(ResultByte))));
-	});
+		core->ActivityManager().UpdateActivity(activity, [](discord::Result result)
+		{
+			uint8 ResultByte = (uint8)result;
+			DiscordObjectInstance->OnLargeImageSet.Broadcast(static_cast<EDiscordReturnResult>(ResultByte));
+			LogDisplay(FString::Printf(TEXT("Large Image Set Result: %s"), *GetDiscordResultString(static_cast<EDiscordReturnResult>(ResultByte))));
+		});
+	}
 }
 
 void UDiscordObject::StartDiscordTimer()
 {
 	activity.GetTimestamps().SetStart(FDateTime::UtcNow().ToUnixTimestamp());
-	core->ActivityManager().UpdateActivity(activity, [](discord::Result result)
+	if (core)
 	{
-		uint8 ResultByte = (uint8)result;
-		DiscordObjectInstance->OnTimerStart.Broadcast(static_cast<EDiscordReturnResult>(ResultByte));
-		LogDisplay(FString::Printf(TEXT("Timer Start Result: %s"), *GetDiscordResultString(static_cast<EDiscordReturnResult>(ResultByte))));
-	});
+		core->ActivityManager().UpdateActivity(activity, [](discord::Result result)
+		{
+			uint8 ResultByte = (uint8)result;
+			DiscordObjectInstance->OnTimerStart.Broadcast(static_cast<EDiscordReturnResult>(ResultByte));
+			LogDisplay(FString::Printf(TEXT("Timer Start Result: %s"), *GetDiscordResultString(static_cast<EDiscordReturnResult>(ResultByte))));
+		});
+	}
 }
 
 void UDiscordObject::StopDiscordTimer()
 {
 	DiscordObjectInstance->bTimerStarted = false;
 	activity.GetTimestamps().SetEnd(FDateTime::UtcNow().ToUnixTimestamp());
-	core->ActivityManager().UpdateActivity(activity, [](discord::Result result)
+	if (core)
 	{
-		uint8 ResultByte = (uint8)result;
-		DiscordObjectInstance->OnTimerEnd.Broadcast(static_cast<EDiscordReturnResult>(ResultByte));
-		LogDisplay(FString::Printf(TEXT("Timer End Result: %s"), *GetDiscordResultString(static_cast<EDiscordReturnResult>(ResultByte))));
-	});
+		core->ActivityManager().UpdateActivity(activity, [](discord::Result result)
+		{
+			uint8 ResultByte = (uint8)result;
+			DiscordObjectInstance->OnTimerEnd.Broadcast(static_cast<EDiscordReturnResult>(ResultByte));
+			LogDisplay(FString::Printf(TEXT("Timer End Result: %s"), *GetDiscordResultString(static_cast<EDiscordReturnResult>(ResultByte))));
+		});
+	}
 }
 
 void UDiscordObject::Internal_CreateDiscordObject(const FString& InClientID, const bool bRequireDiscordRunning, const bool bStartElapsedTimer)
 {	
 	discord::Result result = discord::Core::Create(FCString::Atoi64(*InClientID), bRequireDiscordRunning ? DiscordCreateFlags_Default : DiscordCreateFlags_NoRequireDiscord, &core);
-	DiscordObjectInstance->bCanTick = true;
-	LogDisplay("Discord object created.");
-
-	if (bStartElapsedTimer)
+	if (result == discord::Result::Ok)
 	{
-		DiscordObjectInstance->StartDiscordTimer();
+		DiscordObjectInstance->bCanTick = true;
+		LogDisplay("Discord object created.");
+
+		if (bStartElapsedTimer)
+		{
+			DiscordObjectInstance->StartDiscordTimer();
+		}
 	}
 }
 
